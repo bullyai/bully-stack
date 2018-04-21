d6292655f68a7c6cd81901140c40c3b93d7c640f
Bennett Hardwick <bennetthardwick@icloud.com>
2018-04-21 16:43:47 +1000

    Fixed merge

--- client/components/AddUser.tsx
+++ client/components/AddUser.tsx
@@ -16,15 +16,14 @@ interface UserInfo {
     licenseNumber?: string;
 }
 
-
-
-export default class Navbar extends React.Component<{nav: Object},{error: any, isLoaded: boolean, users: UserInfo[], selectedUser?: UserInfo, ensecurities: any[], insecurities: string[] }> {
+export default class Navbar extends React.Component<{nav: Object},{error: any, isLoaded: boolean, isSubmitted: boolean, users: UserInfo[], selectedUser?: UserInfo, ensecurities: any[], gender: string, insecurities: string[] }> {
 
     constructor(props) {
         super(props);
         this.state = {
             error: undefined,
             isLoaded: false,
+            isSubmitted: false,
             users: [],
             selectedUser: undefined,
             ensecurities: [
@@ -40,7 +39,8 @@ export default class Navbar extends React.Component<{nav: Object},{error: any, i
                 { i: "spouse", checked: false },
                 { i: "personality", checked: false }
             ],
-            insecurities: []
+            insecurities: [],
+            gender: 'male',
         };
     }
 
@@ -88,6 +88,10 @@ export default class Navbar extends React.Component<{nav: Object},{error: any, i
         this.state.ensecurities.map(x => {
             if (x.i == e.target.name) x.checked = !x.checked;
         });
+    }
+
+    readonly genderSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        this.setState({ gender: e.target.value });
     };
 
     readonly sendNew = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -107,7 +111,13 @@ export default class Navbar extends React.Component<{nav: Object},{error: any, i
             .then(result => {
                 if (!result.success) throw result.error;
 
-                // todo: go to another page?
+                this.setState({
+                    error: undefined,
+                    isSubmitted: true,
+                    selectedUser: undefined,
+                    gender: 'male',
+                    insecurities: []
+                });
             })
             .catch(error => {
                 this.setState({ error });
@@ -126,6 +136,9 @@ export default class Navbar extends React.Component<{nav: Object},{error: any, i
                     </div>
                     <form className="addUserForm">
                         <div className="row">
+                            { this.state.isSubmitted ? <div className="alert alert-success"><span>Yeet</span></div> : null }
+                        </div>
+                        <div className="row">
                             <div className="col-12 col-md-6">
                                     <div className="form-group">
                                         <label>User's Name</label>
@@ -136,17 +149,21 @@ export default class Navbar extends React.Component<{nav: Object},{error: any, i
                                                     return <option value={user.id}>{user.name}</option>
                                                 })
                                             }
-                                            {/* <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option> */}
                                         </select>
                                     </div>
                                     <hr />
+                                    <div className="form-group">
+                                        <label>Gender</label>
+                                        <select name="gender" className="form-control" onChange={this.genderSelectChange}>
+                                            <option value="male">Male</option>
+                                            <option value="female">Female</option>
+                                            <option value="other">Other</option>
+                                        </select>
+                                    </div>
                                     {this.state.ensecurities.map(x => x.i).map(x => <span>
                                         <input name={x} id={`in-${x}`} type="checkbox" onChange={(e) => this.handleInsecurityChange(e, x)} className="tgl tgl-flip"/>
                                         <label data-tg-off={x} data-tg-on={x} htmlFor={`in-${x}`}className="tgl-btn"></label>
-                                    </span>)}
+                                    </span>)}                                
                             </div>
                             <div className="col-12 col-md-6">
                                 <img className="img-fluid rounded mx-auto d-block" src={this.state.selectedUser && this.state.selectedUser.photo ? this.state.selectedUser.photo : "http://via.placeholder.com/350x350"} />
